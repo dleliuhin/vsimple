ec251015ab3ae52e60d98f41a978049bb0b34778
Alexander Gromtsev <agromtcev@b2broker.net>
2021-05-27 17:31:45 +0300

    Added vcompiler_traits.

---
+++ cmake/vcompiler_traits.cmake
@@ -0,0 +1,1 @@
+include( ${CMAKE_CURRENT_LIST_DIR}/../vcompiler_traits/vcompiler_traits.cmake )
---
+++ vcompiler_traits/vcompiler_traits.cmake
@@ -0,0 +1,35 @@
+#########################################################################################
+##
+##  GNU Lesser General Public License Usage
+##  This file may be used under the terms of the GNU Lesser General Public License
+##  version 3 as published by the Free Software Foundation and appearing in the file
+##  LICENSE.LGPL3 included in the packaging of this file. Please review the following
+##  information to ensure the GNU Lesser General Public License version 3 requirements
+##  will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
+##
+#########################################################################################
+# vcompiler_traits.cmake
+
+#========================================================================================
+
+if ( vcompiler_traits_included )
+    return()
+endif()
+set( vcompiler_traits_included TRUE )
+
+#========================================================================================
+
+message( STATUS "=== About include vcompiler_traits... ===" )
+
+include_directories( ${CMAKE_CURRENT_LIST_DIR} )
+
+set( HEADERS ${HEADERS} ${CMAKE_CURRENT_LIST_DIR}/vcompiler_traits.h         )
+
+set( HEADERS ${HEADERS} ${CMAKE_CURRENT_LIST_DIR}/details/vplatform.h        )
+set( HEADERS ${HEADERS} ${CMAKE_CURRENT_LIST_DIR}/details/vendian.h          )
+set( HEADERS ${HEADERS} ${CMAKE_CURRENT_LIST_DIR}/details/vpretty_function.h )
+
+message( STATUS "=== vcompiler_traits included ===" )
+
+# vcompiler_traits.cmake
+#========================================================================================
